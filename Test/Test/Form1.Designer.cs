1f53a79f74eb6d859710daeefbcea0f22f4db42c
moxzone <moxzone@hotmail.com>
2016-03-01 14:04:36 +0800

    a

--- Test/Test/Form1.Designer.cs
+++ Test/Test/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 226);
+            this.button1.Location = new System.Drawing.Point(181, 210);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(91, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
